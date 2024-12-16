70b691fe06f87e6c68120a6df07b88d46ab08be8
ibdj <ibdjacobsen@gmail.com>
2024-12-16 10:12:54 +0100

    reading data, matching with GBIF, checking fuzzy taxon

--- scripts/processing_taxon_data.R
+++ scripts/processing_taxon_data.R
@@ -2,13 +2,24 @@
 
 #### loading packages ####
 install.packages("pacman")
-pacman::p_load(tidyverse,googlesheets4, rgbif, ids, lubridate, inborutils) 
+pacman::p_load(tidyverse,googlesheets4, rgbif, ids, lubridate, devtools) 
+devtools::install_github("inbo/inborutils")
+library(inborutils)
 
 #### reading the file ####
 
-xx <- read_sheet('https://docs.google.com/spreadsheets/d/1enf76zz9EBApZVkqWaT1F826Y46OJ5NTKqB8nbGrtug/edit#gid=1166476565', sheet = '', skip = 1)
-
+xx <- read_sheet('https://docs.google.com/spreadsheets/d/1Zeqozv4IKLRNfixj5c8vwsZQf6BFwduJu7DyWTVSfsw/edit?gid=0#gid=0', sheet = 'taxa')
+  
 #### matching with gbif backbone ####
+
+gbif_matched <- xx |> 
+  gbif_species_name_match(name = "scientificname") 
+
+#### checking fussy taxa ####
+
+fussy <- gbif_matched |> 
+  filter(matchType == "FUZZY") |> 
+  select(family...1,scientificname, scientificName,pub,page,)
 
 #### writing the file ####
 
