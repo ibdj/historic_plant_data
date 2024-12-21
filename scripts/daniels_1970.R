97162dd44d02c6b0bcc8eb5d3d2de7e6c7815337
ibdj <ibdjacobsen@gmail.com>
2024-12-21 21:34:34 +0100

    pivoting data longer

--- scripts/daniels_1970.R
+++ scripts/daniels_1970.R
@@ -1,3 +1,8 @@
 
 
-# https://docs.google.com/spreadsheets/d/1adm-3HKTyeDMY-WPOWWSKN37hGuj5EWNMjJIh3D6tkc/edit?gid=0#gid=0
+xy <- read_sheet('https://docs.google.com/spreadsheets/d/1adm-3HKTyeDMY-WPOWWSKN37hGuj5EWNMjJIh3D6tkc/edit?gid=0#gid=0', sheet = 'data')
+
+xy_longer <- xy |> 
+  pivot_longer(cols = 4:10, names_to = "location", values_to = "date") |> 
+  drop_na()
+  
