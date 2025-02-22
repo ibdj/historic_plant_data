fe73abf1a28af38ddef4eed23ae1ef1a2d074159
ibdj <ibdjacobsen@gmail.com>
2025-02-22 16:28:50 -0200

    new script for vaage_1932_eirikraudesland

---
+++ scripts/vaage_1932_eirikraudesland.R
@@ -0,0 +1,4 @@
+taxa <- read_sheet('https://docs.google.com/spreadsheets/d/12SAJkY1w_2K00zcimrtjUWynfprDtKdFRTY03UFPyVM/edit?gid=1761157836#gid=1761157836', sheet = 'taxa')
+
+locations <- read_sheet('https://docs.google.com/spreadsheets/d/12SAJkY1w_2K00zcimrtjUWynfprDtKdFRTY03UFPyVM/edit?gid=1761157836#gid=1761157836', sheet = 'locations')
+
