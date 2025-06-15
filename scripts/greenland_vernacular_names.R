251bc3b2ba1b1d26bd78689ddae23b1de0e4c8d5
ibdj <ibdjacobsen@gmail.com>
2025-06-15 22:37:44 +0200

    code to seperate each grl name in to ONE row

--- scripts/greenland_vernacular_names.R
+++ scripts/greenland_vernacular_names.R
@@ -16,6 +16,26 @@ unique <- Nunatta_naasui |>
   distinct(taxon) |> 
   rename("name" = "taxon")
 
+#### seperating to one gl name pr row####
+
+Nunatta_naasui$grl_ref1 <- trimws(Nunatta_naasui$grl_ref1)
+
+max_splits <- max(sapply(strsplit(as.character(Nunatta_naasui$grl_ref1), ","), length))
+
+col_names <- paste0("col", seq_len(max_splits))
+
+seperate <- separate(Nunatta_naasui, grl_ref1 , into = col_names, sep = ",", fill = "right")
+
+Nunatta_naasui_longer <- seperate |> 
+  pivot_longer(cols = col1:col3, names_to = "placering", values_to = "grl_name", values_drop_na = FALSE)
+
+Nunatta_naasui_longer$grl_name <- trimws(Nunatta_naasui_longer$grl_name)
+
+filtered <- Nunatta_naasui_longer |> 
+  filter(!is.na(grl_name))
+
+#### match to gbif ####
+
 gbif_matched <- unique |> 
   name_backbone_checklist("name") |> 
   mutate(matchType = as.factor(matchType),
