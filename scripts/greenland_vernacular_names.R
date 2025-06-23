07986a7d1ee01b0b7a3230563741d3c994ad04b6
ibdj <ibdjacobsen@gmail.com>
2025-06-23 07:59:24 +0200

    adjusted the match

--- scripts/greenland_vernacular_names.R
+++ scripts/greenland_vernacular_names.R
@@ -27,12 +27,12 @@ col_names <- paste0("col", seq_len(max_splits))
 seperate <- separate(Nunatta_naasui, grl_ref1 , into = col_names, sep = ",", fill = "right")
 
 Nunatta_naasui_longer <- seperate |> 
-  pivot_longer(cols = col1:col3, names_to = "placering", values_to = "grl_name", values_drop_na = FALSE)
+  pivot_longer(cols = col1:col3, names_to = "placering", values_to = "grl_name", values_drop_na = TRUE)
 
-Nunatta_naasui_longer$grl_name <- trimws(Nunatta_naasui_longer$grl_name)
+Nunatta_naasui_longer$grl_name <- trimws(Nunatta_naasui_longer$grl_name) 
 
 filtered <- Nunatta_naasui_longer |> 
-  filter(!is.na(grl_name))
+  filter(!is.na("grl_name")) 
 
 #### match to gbif ####
 
@@ -43,6 +43,11 @@ gbif_matched <- unique |>
          )
 
 summary(gbif_matched)
+
+#### joining the gbid match and data file ####
+joined <- filtered |> 
+  mutate(scientificName = taxon) |> 
+  left_join(gbif_matched, by = "scientificName")
 
 #### list of colomns in the extension of vernacular names #####
 #https://rs.gbif.org/extension/gbif/1.0/vernacularname.xml
