a3b6aa87b848e0c3c847db963333c180845de542
ibdj <ibdjacobsen@gmail.com>
2025-07-07 10:07:55 -0100

    exporting the gbif mated taxon list to google sheets instead of processing here

--- scripts/porsild_1910_hareisland.R
+++ scripts/porsild_1910_hareisland.R
@@ -4,12 +4,23 @@
 
 library(googlesheets4)
 library(tidyverse)
-install.packages("rgbif")
+remotes::install_github("ropensci/rgbif")
 library(rgbif)
 
+packageVersion("rgbif")
 
 #### importing #####
 
-porsild1910 <- read_sheet('https://docs.google.com/spreadsheets/d/1raYqt1BoLer6AP-bACSlEvHvxYcpnEGdKOXSew6oMSo/edit?gid=0#gid=0', sheet = 'taxa') 
+porsild1910 <- read_sheet('https://docs.google.com/spreadsheets/d/1raYqt1BoLer6AP-bACSlEvHvxYcpnEGdKOXSew6oMSo/edit?gid=0#gid=0', sheet = 'taxa') |> select("name")
 
-matched_names <- porsild1910 |> name_backbone_checklist("taxon")
+matched_names <- name_backbone_checklist(porsild1910, "taxon")
+sapply(matched_names, is.list)
+
+nas <-  matched_names |> 
+  filter(is.na(scientificName))
+
+sheet_write(
+  data = matched_names,
+  ss = "https://docs.google.com/spreadsheets/d/1raYqt1BoLer6AP-bACSlEvHvxYcpnEGdKOXSew6oMSo/edit#gid=0",
+  sheet = "matched_names"
+)
