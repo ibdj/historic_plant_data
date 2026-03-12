a20754be688b23bac4ad194ae92cc50671e24fa0
ibdj <ibdjacobsen@gmail.com>
2026-03-12 16:00:20 -0200

    new script for sørensen 1933

---
+++ scripts/sørensen_1933_eastgreenland.R
@@ -0,0 +1,7 @@
+#### reading packages ##########################################################################################################
+
+if (!require("pacman")) install.packages("pacman")
+devtools::install_github("inbo/inborutils")
+pacman::p_load(tidyverse,googlesheets4, rgbif, ids, lubridate, devtools, inborutils) 
+
+
