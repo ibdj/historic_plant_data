41244eb388d97152bc6b41b09e9c1857c5ab835f
ibdj <ibdjacobsen@gmail.com>
2026-03-18 20:55:52 -0200

    adjusting the import and starting gbif match

--- scripts/sørensen_1933_eastgreenland.R
+++ scripts/sørensen_1933_eastgreenland.R
@@ -8,10 +8,14 @@ pacman::p_load(tidyverse,googlesheets4, rgbif, ids, lubridate, devtools, inborut
 #### reading the data from google sheets ########################################################################
 taxa <- read_sheet('https://docs.google.com/spreadsheets/d/1S6pmV9dO5dKpk1sDENTa5-XmCY41WaJcFRCJLYRG8xg/edit?gid=768492226#gid=768492226', sheet = 'taxa')
 
+taxa <- taxa |> 
+  mutate(across(everything(), as.character))
+
 taxa_pivot <- taxa |> 
   pivot_longer(cols = 5:ncol(taxa), names_to = "verbatimLocation", values_to = "presence") |> 
   select(scientificName,verbatimLocation,presence) |> 
-  drop_na()
+  drop_na() |> 
+  filter(presence != "NULL")
 
 loc <- read_sheet('https://docs.google.com/spreadsheets/d/1S6pmV9dO5dKpk1sDENTa5-XmCY41WaJcFRCJLYRG8xg/edit?gid=768492226#gid=768492226', sheet = 'loc')
 
@@ -19,3 +23,10 @@ taxa_coordinates <- taxa_pivot |>
   left_join(loc, by = "verbatimLocation")
 
 #### matching with gbif ####
+
+match_list <- taxa |> 
+  select(scientificName) |> 
+  name_backbone_checklist("taxon") |> 
+  select(usageKey,scientificName, verbatim_name, matchType, confidence)
+
+str(match_list)
