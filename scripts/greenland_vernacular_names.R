6edf30ab52717df4c8ab35b780675c48fcace3e7
ibdj <ibdjacobsen@gmail.com>
2026-03-26 10:32:41 -0200

    minor edits

--- scripts/greenland_vernacular_names.R
+++ scripts/greenland_vernacular_names.R
@@ -1,12 +1,12 @@
-#### greenlandic names #####
+#### greenlandic names #########################################################
 
-#### packages #####
+#### packages ##################################################################
 
 library(googlesheets4)
 library(tidyverse)
 library(rgbif)
 
-#### importing #####
+#### importing #################################################################
 
 Nunatta_naasui <- read_sheet('https://docs.google.com/spreadsheets/d/1-SLBF6wwNXc1z3znjUNZuZQQ8XXx_iplr4sumrnwOn4/edit?gid=1576015470#gid=1576015470', sheet = 'Nunatta_naasui')
 
@@ -16,18 +16,18 @@ unique <- Nunatta_naasui |>
   distinct(taxon) |> 
   rename("name" = "taxon")
 
-#### seperating to one gl name pr row####
+#### seperating to one gl name pr row ##########################################
 
-Nunatta_naasui$grl_ref1 <- trimws(Nunatta_naasui$grl_ref1)
+Nunatta_naasui$source <- trimws(Nunatta_naasui$source)
 
-max_splits <- max(sapply(strsplit(as.character(Nunatta_naasui$grl_ref1), ","), length))
+max_splits <- max(sapply(strsplit(as.character(Nunatta_naasui$source), ","), length))
 
 col_names <- paste0("col", seq_len(max_splits))
 
-seperate <- separate(Nunatta_naasui, grl_ref1 , into = col_names, sep = ",", fill = "right")
+seperate <- separate(Nunatta_naasui, source, into = col_names, sep = ",", fill = "right")
 
 Nunatta_naasui_longer <- seperate |> 
-  pivot_longer(cols = col1:col3, names_to = "placering", values_to = "grl_name", values_drop_na = TRUE) |> 
+  pivot_longer(cols = col1:ncol(seperate), names_to = "placering", values_to = "grl_name", values_drop_na = TRUE) |> 
   mutate(name = taxon,
          grl_name = trimws(grl_name))
 
@@ -40,7 +40,7 @@ matched_list <- Nunatta_naasui_longer |>
   left_join(gbif_matched, by = "taxon") |> 
   drop_na("name")
 
-#### list of colomns in the extension of vernacular names #####
+#### list of colomns in the extension of vernacular names ######################
 #https://rs.gbif.org/extension/gbif/1.0/vernacularname.xml
 # these are the colomns to be included for the extension of vernacularnames. 
 # I have prioritised the list. First section is the ones I think will be included.
