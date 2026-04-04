49a2dabf200cf3967d1b74052327f208457d5ee7
ibdj <ibdjacobsen@gmail.com>
2026-04-04 22:11:35 -0100

    importing the data

--- scripts/pedersen_1965_ivigtut.R
+++ scripts/pedersen_1965_ivigtut.R
@@ -6,3 +6,14 @@ library(googlesheets4)
 library(tidyverse)
 library(rgbif)
 library(janitor)
+
+#### importing #################################################################
+
+taxa <- read_sheet('https://docs.google.com/spreadsheets/d/1sHTPbp2SjKqTGQ6SdDBFzdyOZTy5h647GgQnNU1TB98/edit?gid=0#gid=0', sheet = 'taxa') |> 
+  clean_names()
+
+names(taxa)
+
+unique <- taxa |> 
+  distinct(taxon) |> 
+  rename("name" = "taxon")
