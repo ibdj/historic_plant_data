01b641b6e5d71cb3e94d3d20860319e7dcf91c7d
ibdj <ibdjacobsen@gmail.com>
2026-04-04 22:28:31 -0100

    generated the sci_name

--- scripts/pedersen_1965_ivigtut.R
+++ scripts/pedersen_1965_ivigtut.R
@@ -14,6 +14,23 @@ taxa <- read_sheet('https://docs.google.com/spreadsheets/d/1sHTPbp2SjKqTGQ6SdDBF
 
 names(taxa)
 
+taxa <- taxa |> 
+  mutate(
+    rank = coalesce(rank, ""),
+    intraspecific = coalesce(intraspecific, ""),
+    author = coalesce(author, ""),
+    
+    sci_name = str_squish(paste(
+      genus,
+      epitet,
+      if_else(intraspecific != "" & rank != "",
+              paste(rank, intraspecific), ""),
+      author
+    ))
+  )
+
+summary(taxa)
+
 unique <- taxa |> 
-  distinct(taxon) |> 
-  rename("name" = "taxon")
+  distinct(sci_name) |> 
+  rename("name" = "sci_name")
