246a5a20416572799b82fc86e9d79719181faf46
ibdj <ibdjacobsen@gmail.com>
2026-04-04 22:09:56 -0100

    new script for pedersen 1965 bot. tids. on Ivigtut

---
+++ scripts/pedersen_1965_ivigtut.R
@@ -0,0 +1,8 @@
+#### greenlandic names #########################################################
+
+#### packages ##################################################################
+
+library(googlesheets4)
+library(tidyverse)
+library(rgbif)
+library(janitor)
