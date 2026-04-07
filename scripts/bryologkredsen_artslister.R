40aed299f23a9bd59956a71bd3c62ef2f9c5d31d
ibdj <ibdjacobsen@gmail.com>
2026-04-07 22:55:59 -0100

    adding split to rank

--- scripts/bryologkredsen_artslister.R
+++ scripts/bryologkredsen_artslister.R
@@ -22,6 +22,10 @@ meta_data <- read_sheet('https://docs.google.com/spreadsheets/d/1sOgdCCPdk0qTtko
 
 names(meta_data)
 
+lokaliteter <- meta_data |> 
+  group_by(lokalitetsnavn,lat) |> 
+  reframe(count = n())
+
 str(meta_data)
 summary(meta_data)
 
@@ -51,7 +55,8 @@ all_data
 
 taxon_list <- all_data |> 
   group_by(taxon) |> 
-  reframe(count = n())
+  reframe(count = n()) |> 
+  drop_na()
 
 stats <- all_data |> 
   group_by(filnavn) |> 
@@ -105,11 +110,45 @@ compare_column_typos <- function(df, col_name, threshold = 0.8) {
 typos <- compare_column_typos(taxon_list, "taxon", threshold = 0.8)
 typos
 
-matches <- all_data[all_data$taxon == "Ptilidium pulchrerrimum", ]
+matches <- all_data[all_data$taxon == "Chilocsyphus latifolius", ]
 matches
 
-matches2 <- all_data[grepl("^Amblystegium varium / Hygroa", all_data$taxon), ]
+matches2 <- all_data[grepl("Syntrichia ruralis/calcicola", all_data$taxon), ]
 matches2
 
 matches3 <- all_data[grepl("2002-10-05-Lønborg Hede-Mergelgrav ved Lø", all_data$filnavn), ]
 matches3
+
+#### splitting to rank #########################################################
+# Updated regex pattern to include 'sect' and 'subsect'
+# ^([[:alpha:]]+) : Capture Genus
+# (\s+[[:alpha:]-]+)? : Optional Species epithet (allows hyphens)
+# (\s+(spp?\\.?))?: Optional "sp." or "spp."
+# (\s+(ssp|subsp|var|f|fo|sect|subsect)\\.?)? : Optional Rank (adds 'sect' and 'subsect')
+# (\s+[[:alpha:]]+)?$ : Optional Infraspecific epithet (or Section name)
+
+
+split <- taxon_list |> 
+  mutate(
+    # Updated regex pattern to include 'sect' and 'subsect'
+    match_result = str_match(
+      str_squish(taxon),
+      "^([[:alpha:]]+)(?:\\s+([[:alpha:]-]+))?(?:\\s+(spp?\\.?))?(?:\\s+(ssp|subsp|var|f|fo|sect|subsect)\\.?)?(?:\\s+([[:alpha:]]+))?$"
+    )
+  ) |> 
+  mutate(
+    genus = match_result[, 2],
+    epithet = match_result[, 3],
+    rank = case_when(
+      !is.na(match_result[, 4]) ~ match_result[, 4],  # sp. or spp.
+      !is.na(match_result[, 5]) ~ match_result[, 5],  # ssp, var, f, fo, sect, subsect
+      TRUE ~ NA_character_
+    ),
+    infraspecific_epithet = match_result[, 6]
+  ) |> 
+  mutate(
+    rank = str_to_lower(rank),
+    # Fill empty rank cells with "species"
+    rank = if_else(is.na(rank), "species", rank)
+  ) |> 
+  select(genus, epithet, rank, infraspecific_epithet)
