50f2bb10389b5ccedaa31e1c5d4071ddb7974cae
ibdj <ibdjacobsen@gmail.com>
2026-04-20 22:43:34 -0100

    organised the script

--- scripts/bryologkredsen_artslister.R
+++ scripts/bryologkredsen_artslister.R
@@ -11,14 +11,15 @@ library(stringdist) # to find potential typos
 
 #### importing #################################################################
 
-meta_data <- read_sheet('https://docs.google.com/spreadsheets/d/1sOgdCCPdk0qTtko0bapGjsNSxdyQULbShGpGfimuuqw/edit?gid=0#gid=0', sheet = 'taxa') |> 
+meta_data <- read_sheet('https://docs.google.com/spreadsheets/d/1sOgdCCPdk0qTtko0bapGjsNSxdyQULbShGpGfimuuqw/edit?gid=0#gid=0', sheet = 'meta') |> 
   drop_na(lokalitet) |> 
   mutate(region = as.factor(region),
          area = as.factor(area),
          observer = as.factor(observer))
 
-#new_sheet <- gs4_create("my-sheet-name")
-#sheet_write(data = all_data, ss = new_sheet, sheet = "Sheet1")
+koordinator <- read_sheet('https://docs.google.com/spreadsheets/d/1sOgdCCPdk0qTtko0bapGjsNSxdyQULbShGpGfimuuqw/edit?gid=0#gid=0', sheet = 'koordinator') |> 
+  #drop_na(lokalitet) |> 
+  mutate()
 
 names(meta_data)
 
@@ -47,11 +48,65 @@ all_data <- files %>%
       strip.white = TRUE     # Equivalent to trim_ws
     ) %>%
       rename(taxon = 1) %>%
-      mutate(filnavn = basename(.x) %>% 
+      mutate(tur_id = basename(.x) %>% 
                str_remove("\\.csv$"))
-  })
+  }) |> 
+  mutate(
+    # Updated regex pattern to include 'sect' and 'subsect'
+    match_result = str_match(
+      str_squish(taxon),
+      "^([[:alpha:]]+)(?:\\s+([[:alpha:]-]+))?(?:\\s+(spp?\\.?))?(?:\\s+(ssp|subsp|var|f|fo|sect|subsect)\\.?)?(?:\\s+([[:alpha:]]+))?$"
+    )
+  ) |> 
+  mutate(
+    genus = match_result[, 2],
+    epithet = match_result[, 3],
+    rank = case_when(
+      !is.na(match_result[, 4]) ~ match_result[, 4],  # sp. or spp.
+      !is.na(match_result[, 5]) ~ match_result[, 5],  # ssp, var, f, fo, sect, subsect
+      TRUE ~ NA_character_
+    ),
+    infraspecific_epithet = match_result[, 6]
+  ) |> 
+  mutate(
+    rank = str_to_lower(rank),
+    # Fill empty rank cells with "species"
+    rank = if_else(is.na(rank), "species", rank)
+  ) |> 
+  mutate(taxon = paste0(genus, ifelse(is.na(epithet), "", paste0(" ", epithet)))) |>
+  mutate(across(everything(), ~ifelse(trimws(.) == "", NA, .))) |> 
+  rename(habitat_species = habitat)
 
 all_data
+
+names(all_data)
+
+split <- all_data |> 
+  mutate(
+    # Updated regex pattern to include 'sect' and 'subsect'
+    match_result = str_match(
+      str_squish(taxon),
+      "^([[:alpha:]]+)(?:\\s+([[:alpha:]-]+))?(?:\\s+(spp?\\.?))?(?:\\s+(ssp|subsp|var|f|fo|sect|subsect)\\.?)?(?:\\s+([[:alpha:]]+))?$"
+    )
+  ) |> 
+  mutate(
+    genus = match_result[, 2],
+    epithet = match_result[, 3],
+    rank = case_when(
+      !is.na(match_result[, 4]) ~ match_result[, 4],  # sp. or spp.
+      !is.na(match_result[, 5]) ~ match_result[, 5],  # ssp, var, f, fo, sect, subsect
+      TRUE ~ NA_character_
+    ),
+    infraspecific_epithet = match_result[, 6]
+  ) |> 
+  mutate(
+    rank = str_to_lower(rank),
+    # Fill empty rank cells with "species"
+    rank = if_else(is.na(rank), "species", rank)
+  ) |> 
+  select(taxon, syn, genus, epithet, rank, notes, infraspecific_epithet, filnavn) |> 
+  left_join(meta_data, by = "filnavn") |> 
+  left_join(koordinator, by = "lokalitetsnavn")
 
 taxon_list <- all_data |> 
   group_by(taxon) |> 
@@ -124,14 +179,6 @@ matches3 <- all_data[grepl("2002-10-05-Lønborg Hede-Mergelgrav ved Lø", all_da
 matches3
 
 #### splitting to rank #########################################################
-# Updated regex pattern to include 'sect' and 'subsect'
-# ^([[:alpha:]]+) : Capture Genus
-# (\s+[[:alpha:]-]+)? : Optional Species epithet (allows hyphens)
-# (\s+(spp?\\.?))?: Optional "sp." or "spp."
-# (\s+(ssp|subsp|var|f|fo|sect|subsect)\\.?)? : Optional Rank (adds 'sect' and 'subsect')
-# (\s+[[:alpha:]]+)?$ : Optional Infraspecific epithet (or Section name)
-
-
 split <- taxon_list |> 
   mutate(
     # Updated regex pattern to include 'sect' and 'subsect'
@@ -158,12 +205,17 @@ split <- taxon_list |>
   select(taxon, genus, epithet, rank, infraspecific_epithet)
 
 taxonlist2 <- split |> 
-  mutate(name = paste0(genus, ifelse(is.na(epithet), "", paste0(" ", epithet)))) |> 
-  select(name)
+  mutate(taxon = paste0(genus, ifelse(is.na(epithet), "", paste0(" ", epithet)))) |> 
+  select(taxon)
 
 gbif_matched2 <- taxonlist2 |> 
   rowwise() |> 
   mutate(
-    backbone_data = list(name_backbone(name))
+    backbone_data = list(name_backbone(taxon))
   ) |> 
   unnest_wider(backbone_data, names_sep = "_")
+
+joined_all <- all_data |> 
+  left_join(meta_data, by = "filnavn") |> 
+  left_join(gbif_matched2, by = "taxon") |> 
+  distinct()
